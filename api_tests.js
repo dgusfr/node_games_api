22abaa7dd1a11385c301b30881f8720982dc8c7d
dgusfr <diegogustavo.fr@gmail.com>
2024-10-19 09:52:32 -0300

    feat(get): error

--- api_tests.js
+++ api_tests.js
@@ -1,8 +1,20 @@
 const axios = require("axios");
 
-const api = axios.create({
+const resquest = axios.create({
   baseURL: "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+async function getAllGames() {
+  try {
+    const response = resquest.get("/games");
+    console.log("Lista dos games:", response.data);
+  } catch (error) {
+    console.error(
+      `Erro ao remover jogo com ID ${id}:`,
+      error.response ? error.response.data : error.message
+    );
+  }
+}
